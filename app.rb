14eab9e8454109e2cc3a17f220fd71574bef43f8
Fenton Taylor <fentontaylor@gmail.com>
2019-10-21 20:23:21 -0600

    Endpoint for projections

--- app.rb
+++ app.rb
@@ -7,6 +7,13 @@ class App < Sinatra::Base
     send_file File.expand_path('index.html', settings.public_folder)
   end
 
+  get '/projections/:position/:week' do
+    pos = params[:position]
+    week = params[:week]
+    response = Faraday.get("https://www.fantasyfootballnerd.com/service/weekly-projections/json/ffsqejxq4cax/#{pos}/#{week}")
+    response.body
+  end
+
   not_found do
     content_type :json
     { status: 404, message: "Resource not found." }.to_json
---
+++ spec/fantasy_nerd_spec.rb
@@ -0,0 +1,7 @@
+require_relative 'spec_helper'
+
+describe 'fantasy nerd spec' do
+  it "should return projections" do
+    binding.pry
+  end
+end
