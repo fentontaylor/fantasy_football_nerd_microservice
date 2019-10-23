37720f714618f298abf2a009b7e47f2670a89931
Fenton Taylor <fentontaylor@gmail.com>
2019-10-22 19:46:17 -0600

    Refactor projections endpoint

--- app.rb
+++ app.rb
@@ -3,19 +3,16 @@ require_relative 'config/boot'
 class App < Sinatra::Base
   register Sinatra::Contrib
 
-  get '/' do
-    send_file File.expand_path('index.html', settings.public_folder)
-  end
-
   get '/projections/:position/:week' do
     pos = params[:position]
     week = params[:week]
-    response = Faraday.get("https://www.fantasyfootballnerd.com/service/weekly-projections/json/#{ENV['FF_NERD_KEY']}/#{pos}/#{week}")
+    path = "/service/weekly-projections/json/#{ENV['FF_NERD_KEY']}/#{pos}/#{week}"
+    response = Faraday.get("https://www.fantasyfootballnerd.com" + path)
     response.body
   end
 
   not_found do
     content_type :json
-    { status: 404, message: "Resource not found." }.to_json
+    { status: 404, message: 'Resource not found.' }.to_json
   end
 end
