cc4b49a01442e2bcdbfeeeaab9ff1d51beb828ba
Fenton Taylor <fentontaylor@gmail.com>
2019-10-22 21:01:51 -0600

    Attempt to set up Capybara, the module isnt loading

--- spec/projection_update_spec.rb
+++ spec/projection_update_spec.rb
@@ -1,7 +1,10 @@
 require 'spec_helper'
+require('capybara/rspec')
+require('./app')
+Capybara.app = Sinatra::Application
 
 describe 'visiting the endpoint /projections/update/:position/:week' do
-  scenario 'should update the database with those records if they do not exist' do
-    
+  it 'should update the database with those records if they do not exist' do
+    visit '/projections/QB/3'
   end
 end
