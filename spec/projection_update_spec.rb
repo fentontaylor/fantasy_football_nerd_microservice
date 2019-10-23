22f20f9893ab3e9f8137a49ad54725715f670556
Fenton Taylor <fentontaylor@gmail.com>
2019-10-22 20:43:47 -0600

    Add capybara gem

---
+++ spec/projection_update_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe 'visiting the endpoint /projections/update/:position/:week' do
+  scenario 'should update the database with those records if they do not exist' do
+    
+  end
+end
