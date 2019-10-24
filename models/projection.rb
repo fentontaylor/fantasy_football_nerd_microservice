5aa02522e8cb4bd91f78212b53a75e9be400e5ce
Fenton Taylor <fentontaylor@gmail.com>
2019-10-24 16:22:40 -0600

    Adds calculations for kickers and defense

--- models/projection.rb
+++ models/projection.rb
@@ -1,11 +1,15 @@
 class Projection < ActiveRecord::Base
   def calculate
     if position == 'K'
+      kicker_score
     elsif position == 'DEF'
+      defense_score
     else
       offense_score
     end
   end
+
+  private
 
   def offense_score
     (passYds * 0.04) +
@@ -16,5 +20,30 @@ class Projection < ActiveRecord::Base
       (rushTD * 6) +
       (recYds * 0.1) +
       (recTD * 6)
+  end
+
+  def kicker_score
+    (fg * 3.2 + xp).to_f
+  end
+
+  def defense_score
+    (defTD * 6) +
+      (defFR * 2) +
+      (defInt * 2) +
+      (defSack * 1) +
+      (defSafety * 2) +
+      points_allowed(defPA)
+  end
+
+  def points_allowed(pts_allowed)
+    case pts_allowed
+    when 0 then 10
+    when 1..6 then 7
+    when 7..13 then 4
+    when 14..20 then 1
+    when 21..27 then 0
+    when 28..34 then -1
+    when 35..150 then -4
+    end
   end
 end
--- spec/models/projection_spec.rb
+++ spec/models/projection_spec.rb
@@ -41,7 +41,7 @@ describe Projection do
 
   describe 'instance_methods' do
     it '#calculate' do
-      hash = {
+      wr = {
         week: '7',
         playerId: '1446',
         position: 'WR',
@@ -73,10 +73,118 @@ describe Projection do
         team: 'ATL'
       }
 
-      projection = Projection.create(hash)
-      result = projection.calculate
+      qb = {
+        week: '7',
+        playerId: '1446',
+        position: 'QB',
+        passAtt: '30.0',
+        passCmp: '22.0',
+        passYds: '295.0',
+        passTD: '2.4',
+        passInt: '0.4',
+        rushAtt: '3.2',
+        rushYds: '8.3',
+        rushTD: '0.0',
+        fumblesLost: '0.0',
+        receptions: '0.0',
+        recYds: '0.0',
+        recTD: '0.0',
+        fg: '0.0',
+        fgAtt: '0.0',
+        xp: '0.0',
+        defInt: '0.0',
+        defFR: '0.0',
+        defFF: '0.0',
+        defSack: '0.0',
+        defTD: '0.0',
+        defRetTD: '0.0',
+        defSafety: '0.0',
+        defPA: '0.0',
+        defYdsAllowed: '0.0',
+        displayName: 'Thrower McGee',
+        team: 'ATL'
+      }
 
-      expect(result).to eq(12.74)
+      defense = {
+        week: "1",
+        playerId: "1043",
+        position: "DEF",
+        passAtt: "0.0",
+        passCmp: "0.0",
+        passYds: "0.0",
+        passTD: "0.0",
+        passInt: "0.0",
+        rushAtt: "0.0",
+        rushYds: "0.0",
+        rushTD: "0.0",
+        fumblesLost: "0.0",
+        receptions: "0.0",
+        recYds: "0.0",
+        recTD: "0.0",
+        fg: "0.0",
+        fgAtt: "0.0",
+        xp: "0.0",
+        defInt: "1.0",
+        defFR: "0.7",
+        defFF: "0.6",
+        defSack: "3.2",
+        defTD: "0.3",
+        defRetTD: "0.0",
+        defSafety: "0.0",
+        defPA: "17.4",
+        defYdsAllowed: "309.5",
+        displayName: "Philadelphia Eagles",
+        team: "PHI"
+      }
+
+      kicker = {
+        week: "1",
+        playerId: "1865",
+        position: "K",
+        passAtt: "0.0",
+        passCmp: "0.0",
+        passYds: "0.0",
+        passTD: "0.0",
+        passInt: "0.0",
+        rushAtt: "0.0",
+        rushYds: "0.0",
+        rushTD: "0.0",
+        fumblesLost: "0.0",
+        receptions: "0.0",
+        recYds: "0.0",
+        recTD: "0.0",
+        fg: "1.9",
+        fgAtt: "1.9",
+        xp: "2.7",
+        defInt: "0.0",
+        defFR: "0.0",
+        defFF: "0.0",
+        defSack: "0.0",
+        defTD: "0.0",
+        defRetTD: "0.0",
+        defSafety: "0.0",
+        defPA: "0.0",
+        defYdsAllowed: "0.0",
+        displayName: "Greg Zuerlein",
+        team: "LAR"
+      }
+
+      wr_projection = Projection.create(wr)
+      qb_projection = Projection.create(qb)
+      defense_projection = Projection.create(defense)
+      kicker_projection = Projection.create(kicker)
+
+      wr_result = wr_projection.calculate
+      expect(wr_result).to eq(12.74)
+
+      qb_result = qb_projection.calculate
+      expect(qb_result).to eq(21.83)
+
+      defense_result = defense_projection.calculate
+      expect(defense_result).to eq(9.4)
+
+      kicker_result = kicker_projection.calculate
+      expect(kicker_result).to eq(8.78)
     end
   end
 end
