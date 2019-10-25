f4a94f9318c52b729ac7a0fafa273e3c537b2cee
Fenton Taylor <fentontaylor@gmail.com>
2019-10-24 20:53:27 -0600

    Fix

--- spec/acceptance_helper.rb
+++ spec/acceptance_helper.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 def stub_projections(pos, week)
   json = File.open("./spec/fixtures/projections_#{pos.downcase}_#{week}.json")
-  root_url = 'https://www.fantasyfootballnerd.com/service/weekly-projections/json/ffsqejxq4cax'
+  root_url = "https://www.fantasyfootballnerd.com/service/weekly-projections/json/#{ENV['FF_NERD_KEY']}"
   stub_request(:get, "#{root_url}/#{pos.upcase}/#{week}")
     .to_return(status: 200, body: json)
 end
