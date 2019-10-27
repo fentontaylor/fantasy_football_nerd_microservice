81e139f6844af645b492145082ebd2884fde5095
Fenton Taylor <fentontaylor@gmail.com>
2019-10-27 14:04:51 -0600

    Add tests and logic for requiring admin key for restricted endpoints

--- spec/features/queries_must_have_a_valid_key_spec.rb
+++ spec/features/queries_must_have_a_valid_key_spec.rb
@@ -4,11 +4,7 @@ describe 'User sends a request to the API' do
   context 'they must provide a valid API key for open access endpoints' do
     before :each do
       stub_projections('WR', 1)
-
-      @open_key = Digest::MD5.hexdigest 'test'
-      open_key_digest = Digest::MD5.hexdigest @open_key
-      Key.create(value: open_key_digest)
-
+      @open_key = create_open_key
       @error_message = { error: 'Invalid API key' }.to_json
     end
 
@@ -35,6 +31,35 @@ describe 'User sends a request to the API' do
       expect(data).to have_key(:week)
       expect(data).to have_key(:playerId)
       expect(data).to have_key(:position)
+    end
+  end
+
+  context 'must provide valid admin API key for restricted endpoints' do
+    before :each do
+      stub_projections('WR', 1)
+      @open_key = create_open_key
+      @admin_key = create_admin_key
+      @error_message = { error: 'Invalid API key' }.to_json
+    end
+
+    scenario 'it sends error message, status 403 if no key given' do
+      get '/projections/update/WR/1'
+
+      expect(last_response).to_not be_successful
+      expect(last_response.body).to eq(@error_message)
+    end
+
+    scenario 'it sends error message, status 403 if no key is given but is wrong access_type' do
+      get "/projections/update/WR/1?key=#{@open_key}"
+
+      expect(last_response).to_not be_successful
+      expect(last_response.body).to eq(@error_message)
+    end
+
+    scenario 'it updates records if key is valid and correct access_type' do
+      get "/projections/update/WR/1?key=#{@admin_key}"
+
+      expect(last_response).to be_successful
     end
   end
 end
